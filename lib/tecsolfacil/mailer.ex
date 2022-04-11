032b2f98c0b031c2bd570114d7bcdc9dd8596ee2
JVMartyns <jvmartyns2015@gmail.com>
2022-04-11 20:51:42 -0300

    first commit

---
+++ lib/tecsolfacil/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Tecsolfacil.Mailer do
+  use Swoosh.Mailer, otp_app: :tecsolfacil
+end
---
+++ lib/tecsolfacil/repo.ex
@@ -0,0 +1,5 @@
+defmodule Tecsolfacil.Repo do
+  use Ecto.Repo,
+    otp_app: :tecsolfacil,
+    adapter: Ecto.Adapters.Postgres
+end
